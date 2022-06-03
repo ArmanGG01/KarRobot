ba6983f1d8076b2a99a8d7c2d1f9fcf1ad25f532
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-06-03 19:05:50 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -30,7 +30,7 @@ async def lastname(steal):
                 r = await conv.get_response()
                 response = await conv.get_response()
             except YouBlockedUserError:
-                await steal.reply("```Error, report to @kenbotsupport```")
+                await steal.reply("```Error, report to @obrolansuar```")
                 return
             if r.text.startswith("Name"):
                 respond = await conv.get_response()
