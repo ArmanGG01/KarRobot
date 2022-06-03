94590a5fc53d93ec42ffe88b357ff475d8fc74bb
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2022-06-03 19:13:38 +0700

    Update afk.py

--- EmikoRobot/modules/afk.py
+++ EmikoRobot/modules/afk.py
@@ -30,7 +30,7 @@ def afk(update: Update, context: CallbackContext):
     if not user:  # ignore channels
         return
 
-    if user.id in [777000, 1694909518]:
+    if user.id in [777000]:
         return
 
     notice = ""
